5e4f88d2025876cac63dfe0ded64c40db8b428d5
venkatamadhav <venkat.madhav.98@gmail.com>
2023-06-13 21:20:55 +0530

    Updated the search and filter

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -47,12 +47,13 @@ const Cards = () => {
   useEffect(() => {
     if (pokemonData) {
       const filteredResults = pokemonData.filter((pokemon) => {
-        if (filter === 'All') {
+        if (filter === 'all' || filter === "All") {
           return (
             pokemon.name.toLowerCase().includes(input) ||
             pokemon.id.toString().includes(input)
           );
-        } else {
+        }
+        else {
           return (
             (pokemon.name.toLowerCase().includes(input) || pokemon.id.toString().includes(input)) &&
             pokemon.types.some((type) => type.type.name.toLowerCase() === filter)
@@ -70,26 +71,26 @@ const Cards = () => {
       </div>
       <div className="flex px-8 sm:px-16 py-4 items-center">
         <label htmlFor="types" className="block mr-6 font-medium text-gray-900 text-lg sm:text-2xl">Type</label>
-        <select name="types" id="types" defaultValue={"All"} onChange={filterchange} value={filter} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1 sm:p-2.5">
-            <option value="All" >All</option>
-            <option value="Normal">Normal</option>
-            <option value="Fire">Fire</option>
-            <option value="Water">Water</option>
-            <option value="Electric">Electric</option>
-            <option value="Grass">Grass</option>
-            <option value="Ice">Ice</option>
-            <option value="Fighting">Fighting</option>
-            <option value="Poison">Poison</option>
-            <option value="Ground">Ground</option>
-            <option value="Flying">Flying</option>
-            <option value="Psychic">Psychic</option>
-            <option value="Bug">Bug</option>
-            <option value="Rock">Rock</option>
-            <option value="Ghost">Ghost</option>
-            <option value="Dragon">Dragon</option>
-            <option value="Dark">Dark</option>
-            <option value="Steel">Steel</option>
-            <option value="Fairy">Fairy</option>
+        <select name="types" id="types" defaultValue="all" value={filter} onChange={filterchange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1 sm:p-2.5">
+            <option value="all" >All</option>
+            <option value="normal">Normal</option>
+            <option value="fire">Fire</option>
+            <option value="water">Water</option>
+            <option value="electric">Electric</option>
+            <option value="grass">Grass</option>
+            <option value="ice">Ice</option>
+            <option value="fighting">Fighting</option>
+            <option value="poison">Poison</option>
+            <option value="ground">Ground</option>
+            <option value="flying">Flying</option>
+            <option value="psychic">Psychic</option>
+            <option value="bug">Bug</option>
+            <option value="rock">Rock</option>
+            <option value="ghost">Ghost</option>
+            <option value="dragon">Dragon</option>
+            <option value="dark">Dark</option>
+            <option value="steel">Steel</option>
+            <option value="fairy">Fairy</option>
         </select>
       </div>
       <div className='flex flex-wrap justify-center mx-auto'>
